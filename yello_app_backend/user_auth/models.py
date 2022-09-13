db1af62df6e9d8fe3010dbcdcc89b26ffe097838
Amie Morales <xamiemmorales@gmail.com>
2022-09-13 09:37:18 -0500

    update

--- yello_app_backend/user_auth/models.py
+++ yello_app_backend/user_auth/models.py
@@ -14,7 +14,7 @@ from django.contrib.auth.models import User
 class Post(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=255)
-    image = models.ImageField(null=True, blank=True)
+    image = models.ImageField(null=True, blank=True, upload_to="image/")
     description = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
