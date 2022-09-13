f0b11acdf626909d6db0736ad18e81a86496ed50
Amie Morales <xamiemmorales@gmail.com>
2022-09-13 15:06:50 -0500

    trying to connect back to front

--- yello_app_backend/user_auth/urls.py
+++ yello_app_backend/user_auth/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('home', views.home, name='home'),
     path('sign-up', views.sign_up, name='sign_up'),
     path('create-post', views.create_post, name='create_post'),
+    path('api/users', views.UserList.as_view(), name='user_list'),
+    path('api/users/<int:pk>', views.UserDetail.as_view(), name='user_detail'),
 ]
