f95a24041a47d851fe116318c73928f85700ddf7
Amie Morales <xamiemmorales@gmail.com>
2022-09-14 09:11:58 -0500

    ready fo rpull

--- yello_app_backend/user_auth/forms.py
+++ yello_app_backend/user_auth/forms.py
@@ -13,4 +13,4 @@ class RegisterForm(UserCreationForm):
 class PostForm(forms.ModelForm):
     class Meta: 
         model = Post
-        fields = ["image", "description"]
+        fields = ["title", "image", "description"]
