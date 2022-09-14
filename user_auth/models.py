4fbb4c1dec9f7dcd507caebf06f161fb9a5a7ff8
MauricioSerrato <mserato99@gmail.com>
2022-09-14 10:44:07 -0500

    still heroku

--- user_auth/models.py
+++ user_auth/models.py
@@ -13,8 +13,8 @@ from django.contrib.auth.models import User
 
 class Post(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
-    image = models.CharField(max_length=2000)
-    description = models.TextField()
+    image = models.CharField(max_length=2000, null=True)
+    description = models.TextField(max_length= 2000)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
