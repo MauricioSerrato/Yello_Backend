f28d3cc949aade79cdb67ec70b139c520d7b5652
MauricioSerrato <mserato99@gmail.com>
2022-09-15 11:55:16 -0500

    fixing

--- user_auth/models.py
+++ user_auth/models.py
@@ -13,8 +13,9 @@ from django.contrib.auth.models import User
 
 class Post(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
-    image = models.CharField(max_length=2000, null=True)
-    description = models.TextField(max_length= 2000)
+    title = models.CharField(max_length=255)
+    image = models.CharField(max_length=2000)
+    description = models.TextField(max_length=300)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
